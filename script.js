4b8194afb6a8733c692cff8dd66db89d1f710ac8
SUGATHIYAGU <143944679+SUGATHIYAGU@users.noreply.github.com>
2024-07-04 19:29:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,423 @@
+const questions={
+    simple:[
+    {
+        question: "which is the largest mammal in the world?",
+        answers:[
+            {text:"shark",correct:false},
+            {text:"Elephant",correct:false},
+            {text:"giraffee",correct:false},
+            {text:"Blue whale",correct:true},
+        ]
+    },
+
+    {
+        question: "Which mountain range forms a natural border between France and Italy ?",
+        answers:[
+            {text:"The Pyrenees",correct:false},
+            {text:"The Alps",correct:true},
+            {text:"Ural Mountains",correct:false},
+            {text:"Dolomites",correct:false},
+        ]
+    },
+
+    {
+        question: "Which planet is known as the Red Planet?",
+        answers:[
+            {text:"Jupiter",correct:false},
+            {text:"Mars",correct:true},
+            {text:"Venus",correct:false},
+            {text:"Saturn",correct:false},
+        ]
+    },
+
+    {
+        question: "Which country is famous for its tulip fields and windmills?",
+        answers:[
+            {text:"Italy",correct:false},
+            {text:"Switzerland",correct:false},
+            {text:"Netherlands",correct:true},
+            {text:"Germany",correct:false},
+        ]
+    },
+    {
+        question: "Which country is known as the Land of the Rising Sun?",
+        answers:[
+            {text:"China",correct:false},
+            {text:"South Korea",correct:false},
+            {text:"Thailand",correct:false},
+            {text:"Japan",correct:true},
+        ]
+    },
+
+    {
+        question: "What is the chemical symbol for the element Gold?",
+        answers:[
+            {text:"Go",correct:false},
+            {text:"Gl",correct:false},
+            {text:"Au",correct:true},
+            {text:"Ag",correct:false},
+        ]
+    },
+
+    {
+        question: "Which famous scientist formulated the theory of relativity?",
+        answers:[
+            {text:"Isaac Newton",correct:false},
+            {text:"Marie Curie",correct:false},
+            {text:"Albert Einstein",correct:true},
+            {text:"Galileo Galilei",correct:false},
+        ]
+    },
+
+    {
+        question: "Who is credited with discovering penicillin?",
+        answers:[
+            {text:"Alexander Fleming",correct:true},
+            {text:"Louis Pasteur",correct:false},
+            {text:"Jonas Salk",correct:false},
+            {text:" Robert Koch",correct:false},
+        ]
+    },
+
+    {
+        question: "How long is the small intestine?",
+        answers:[
+            {text:"Seven meter",correct:true},
+            {text:"Eight meter",correct:false},
+            {text:"Five meter",correct:false},
+            {text:" Nine meter",correct:false},
+        ]
+    },
+    {
+        question: "Which one of these numbers is NOT a factor of 28?",
+        answers:[
+            {text:"7",correct:false},
+            {text:"5",correct:true},
+            {text:"28",correct:false},
+            {text:"14",correct:false},
+        ]
+    }
+    
+    
+],
+medium:[
+
+    {
+        question: "What is the body's most flexible and movable joint?",
+        answers:[
+            {text:"Hinge joints",correct:false},
+            {text:"Condyloid joints",correct:false},
+            {text:"Pivot joints",correct:false},
+            {text:"The shoulder joint",correct:true},
+        ]
+    },
+
+    {
+        question: "On which continent is the Amazon Rainforest located?",
+        answers:[
+            {text:"South America",correct:true},
+            {text:"North America",correct:false},
+            {text:"South Africa",correct:false},
+            {text:"Australia",correct:false},
+        ]
+    },
+    {
+        question: "Which ocean creatures have no teeth?",
+        answers:[
+            {text:"Dolphins",correct:false},
+            {text:"Sae lions",correct:false},
+            {text:"Walruses",correct:false},
+            {text:"Baleen whales",correct:true},
+        ]
+    },
+    {
+        question: "Which part of the brain is responsible for balance and coordination?",
+        answers:[
+            {text:"Cerebellar Cortex",correct:false},
+            {text:"Hypothalamus",correct:false},
+            {text:"cerebellum",correct:true},
+            {text:"Parietal Lobe",correct:false},
+        ]
+    },
+    {
+        question: "Which country won the most medals at the 2020 Tokyo Olympics?",
+        answers:[
+            {text:"The U.S",correct:true},
+            {text:"Japan",correct:false},
+            {text:"Australia",correct:false},
+            {text:"India",correct:false},
+        ]
+    },
+    {
+        question: " How many players are there on a rugby league team?",
+        answers:[
+            {text:"12 player",correct:false},
+            {text:"14 player",correct:false},
+            {text:"13 player",correct:true},
+            {text:"15 player",correct:false},
+        ]
+    },
+    {
+        question: "What animal did Australian soldiers have to fight back in 1932?",
+        answers:[
+            {text:"Lion",correct:false},
+            {text:"Emu",correct:true},
+            {text:"Elephant",correct:false},
+            {text:"Cheetah",correct:false},
+        ]
+    },
+    {
+        question: "Which bird can fly backward?",
+        answers:[
+            {text:"Canary",correct:false},
+            {text:"Buzzard Eagle",correct:false},
+            {text:"Hawks",correct:false},
+            {text:"Humming Bird",correct:true},
+        ]
+    },
+    {
+        question: "In which year did World War II end?",
+        answers:[
+            {text:"1947",correct:false},
+            {text:"1948",correct:false},
+            {text:"1946",correct:false},
+            {text:"1945",correct:true},
+        ]
+    },
+    {
+        question: "How many molecules of oxygen does ozone have?",
+        answers:[
+            {text:"Two",correct:false},
+            {text:"Three",correct:true},
+            {text:"Five",correct:false},
+            {text:"Seven",correct:false},
+        ]
+    },
+],
+hard:[
+
+    {
+        question: " Who was the first person to reach the South Pole?",
+        answers:[
+            {text:"Robert Peary",correct:false},
+            {text:"Roald Amundsen",correct:true},
+            {text:"Ernest Shackleton",correct:false},
+            {text:"Robert Scott",correct:false},
+        ]
+    },
+
+    {
+        question: "Which element has the highest melting point?",
+        answers:[
+            {text:"Tungsten",correct:true},
+            {text:"Carbon",correct:false},
+            {text:"Osmium",correct:false},
+            {text:"Iron",correct:false},
+        ]
+    },
+    {
+        question: "If the radius of a circle is diminished by 10%,then its area is diminished by?",
+        answers:[
+            {text:"19%",correct:true},
+            {text:"10%",correct:false},
+            {text:"25%",correct:false},
+            {text:"36%",correct:false},
+        ]
+    },
+    {
+        question: "The two colors seen at the extreme ends of the pH chart are?",
+        answers:[
+            {text:"Red & Green",correct:false},
+            {text:"Green & Blue",correct:false},
+            {text:"Red & Blue",correct:true},
+            {text:"Orange & Green",correct:false},
+        ]
+    },
+    {
+        question: " Thirty men take 20 days to complete a job working 9 hours a day. How many hours a day should 40 men work to complete the job?",
+        answers:[
+            {text:"8 hrs 15 mins",correct:false},
+            {text:"7 hrs 45 mins",correct:false},
+            {text:"6 hrs 45 mins",correct:true},
+            {text:"9 hrs 12 mins",correct:false},
+        ]
+    },
+    {
+        question: " Find the number which will come in the place of the question mark in the given series 14, 25, 47, 91, 179,",
+        answers:[
+            {text:"255",correct:false},
+            {text:"355",correct:true},
+            {text:"211",correct:false},
+            {text:"321",correct:false},
+        ]
+    },
+    {
+        question: "The two colors seen at the extreme ends of the pH chart are?",
+        answers:[
+            {text:"Red & Green",correct:false},
+            {text:"Green & Blue",correct:false},
+            {text:"Red & Blue",correct:true},
+            {text:"Orange & Green",correct:false},
+        ]
+    },
+    {
+        question: "Choose the unusual word from the below list?",
+        answers:[
+            {text:"Granite",correct:false},
+            {text:"Basalt",correct:false},
+            {text:"Pumice",correct:false},
+            {text:"Sandstone",correct:true},
+        ]
+    },
+    {
+        question: "Where is recognized as the location of the hottest temperature ever recorded on Earth?",
+        answers:[
+            {text:"Mitribah, Kuwait",correct:false},
+            {text:"Yuma, Arizona",correct:false},
+            {text:"Death Valley, California",correct:true},
+            {text:"Key West, Florida",correct:false},
+        ]
+    },
+    {
+        question: "The Arabian Desert is located where in the world?",
+        answers:[
+            {text:"Bolivia",correct:false},
+            {text:" Western Asia",correct:true},
+            {text:"Albania",correct:false},
+            {text:"Eastern Asia",correct:false},
+        ]
+    },
+]
+};
+
+const menusection=document.getElementById("menu");
+const questionsection=document.getElementById("question-section");
+const questionelement=document.getElementById('question');
+const answerbtn=document.getElementById("answer-btn");
+const nextbtn=document.getElementById("next-btn");
+const resulttext=document.getElementById('result-text');
+const resultsection = document.getElementById("result-section");
+const playagainbtn = document.getElementById("play-again");
+const backtohome=document.getElementById("back-home");
+const emoji1section=document.getElementById("emoji-1");
+const face1section=document.getElementsByClassName("face-1");
+const eyes1ection=document.getElementsByClassName("eyes-1");
+const emoji2section=document.getElementById("emoji-2");
+const face2section=document.getElementsByClassName("face-2");
+const eyes2ection=document.getElementsByClassName("eyes-2");
+
+
+let currentquestionindex=0;
+let score=0;
+let currentmode='';
+
+
+document.getElementById("simple-question-btn").addEventListener("click", () =>
+     startquiz('simple'));
+document.getElementById("medium-question-btn").addEventListener("click", () => 
+    startquiz('medium'));
+document.getElementById("hard-question-btn").addEventListener("click", () => 
+    startquiz('hard'));
+
+
+
+function startquiz(mode){
+    currentquestionindex=0;
+    currentmode=mode;
+    score=0;
+    resetstate();
+    menusection.style.display="none";
+    questionsection.style.display="block";
+    resultsection.style.display = "none";
+    playagainbtn.style.display = "none";
+    shownextquestion();
+}
+
+function shownextquestion(){
+    resetstate();
+    let currentquestion=questions[currentmode][currentquestionindex];
+    let questionNO=currentquestionindex+1;
+    questionelement.innerHTML=questionNO+"."+currentquestion.question;
+
+    currentquestion.answers.forEach(answer => {
+        const button=document.createElement("button");
+        button.innerHTML=answer.text;
+        button.classList.add("btn");
+        button.dataset.correct=answer.correct; 
+        button.addEventListener("click",selectoption);
+        answerbtn.appendChild(button);
+        
+    });
+}
+
+function resetstate(){
+    nextbtn.style.display="none";
+    backtohome.style.display="none";
+    while(answerbtn.firstChild){
+        answerbtn.removeChild(answerbtn.firstChild);
+    }
+}
+
+function selectoption(e){
+    const selectbtn=e.target;
+    const iscorrect=selectbtn.dataset.correct==="true";
+    if(iscorrect){
+        selectbtn.classList.add("correct");
+        score++;
+    }else{
+        selectbtn.classList.add("incorrect")
+    }
+    Array.from(answerbtn.children).forEach(button=>{
+        if(button.dataset.correct==="true"){
+            button.classList.add("correct");
+        }
+        button.disabled=true;
+    });
+    nextbtn.style.display='block';
+    backtohome.style.display="block";
+}
+
+
+
+function showResult() {
+    questionsection.style.display = "none";
+    resultsection.style.display = "block";
+    playagainbtn.style.display = "block";
+    if(score>=8){
+        emoji1section.style.display="block";
+        emoji2section.style.display="none";
+        resulttext.innerHTML = `YOU SCORED ${score} OUT OF ${questions[currentmode].length}!! <br/> GREAT!!`;
+    }else if(score>=5){
+        emoji1section.style.display="block";
+        emoji2section.style.display="none";
+        resulttext.innerHTML = `YOU SCORED ${score} OUT OF ${questions[currentmode].length}!! <br/> GOOD JOB!!`;
+
+    }else{
+        emoji2section.style.display="block";
+        emoji1section.style.display="none";
+        resulttext.innerHTML=`YOU SCORED ${score} OUT OF ${questions[currentmode].length} <br/> TRY AGAIN!!`;
+
+    }
+}
+
+nextbtn.addEventListener("click", () => {
+    currentquestionindex++;
+    if (currentquestionindex < questions[currentmode].length) {
+        shownextquestion();
+    } else {
+        showResult();
+    }
+});
+backtohome.addEventListener("click",()=>{
+    menusection.style.display="block";
+    questionsection.style.display="none";
+    emoji1section.style.display="none";
+    emoji2section.style.display="none";
+});
+playagainbtn.addEventListener("click", () => {
+    menusection.style.display="block";
+    resultsection.style.display="none";
+    emoji1section.style.display="none";
+    emoji2section.style.display="none";
+});
+
